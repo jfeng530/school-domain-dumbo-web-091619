42bd8fb08894a4cfdad5ce9f0c9179253a4d3254
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-18 02:28:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,11 +4,14 @@ class School
   attr_accessor :roster
   attr_reader :name 
   
-  ROSTERS = {}
   
   def initialize(name)
     @name = name
     @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster => {student_name=>[grade]}
   end
   
 end
